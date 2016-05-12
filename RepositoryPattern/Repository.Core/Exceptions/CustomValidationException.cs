66d5413f9984e1bbeb2459fa941dac02967d1d19
André Davis <CubicleJockey@users.noreply.github.com>
2016-05-11 21:24:54 -0700

    Starting a Repository Pattern Framework

---
+++ RepositoryPattern/Repository.Core/Exceptions/CustomValidationException.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using Repository.Core.Validators;
+
+namespace Repository.Core.Exceptions
+{
+    public class CustomValidationException : ValidationException
+    {
+        #region Fields
+
+        private readonly IEnumerable<ValidationError> errors;
+
+        #endregion Fields
+
+        #region Overridden
+
+        #region Constructors
+
+        public CustomValidationException(IEnumerable<ValidationError> errors)
+        {
+            this.errors = errors;
+        }
+
+        #endregion Constructors
+
+        #region Overrides of Exception
+
+        /// <summary>
+        /// Gets a message that describes the current exception.
+        /// </summary>
+        /// <returns>
+        /// The error message that explains the reason for the exception, or an empty string ("").
+        /// </returns>
+        public override string Message {
+            get
+            {
+                if(errors != null)
+                {
+                    var msg = new StringBuilder();
+                    foreach(var error in errors)
+                    {
+                        msg.AppendLine(error);
+                    }
+                    return msg.ToString();
+                }
+                return base.Message;
+            } 
+        }
+
+
+        #endregion
+
+        #endregion Overridden
+    }
+}
---
+++ RepositoryPattern/Repository.Core/Validators/ValidationError.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Repository.Core.Validators
+{
+    public class ValidationError
+    {
+        #region Propreties
+
+        public string Message { get; }
+        public string PropertyName { get; }
+
+        #endregion Properties
+
+        #region Constructors
+
+        public ValidationError(string message, string propertyName)
+        {
+            if(string.IsNullOrWhiteSpace(message))
+            {
+                throw new ArgumentException($"{nameof(message)} cannot be null, empty string or whitespace.");
+            }
+            Message = message;
+
+            if(string.IsNullOrWhiteSpace(propertyName))
+            {
+                throw new ArgumentException($"{nameof(propertyName)} cannot be null, empty string or whitepsace.");
+            }
+            PropertyName = propertyName;
+        }
+
+        #endregion Constructors
+    }
+}
